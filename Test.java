0ef74802a037fbac54a5600bc37650d636001c73
unknown <romel85@hotmail.com>
2023-06-03 09:30:47 -0400

    pr-776 third try

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main (String args[])
 	{
 		System.out.println ("First Test Java");
-		System.out.println ("This is second line");
+		System.out.println ("This is second line OK");
 	}
 
 }
