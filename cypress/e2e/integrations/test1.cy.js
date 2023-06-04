b456dacea4f8d86bdef99d3b9b529ec4dbacf15f
rsarkezians <34555480+rsarkezians@users.noreply.github.com>
2023-06-04 15:22:21 -0400

    Pr 776 -- adding all other cypress files. (#6)
    * pr-776 third try
    
    * pr-776 cypress lock json file
    
    * pr-776  adding package.json
    
    * pr-776 rest of cypress files

---
+++ cypress/e2e/integrations/test1.cy.js
@@ -0,0 +1,23 @@
+describe ('sample app', () => {
+
+
+	it ('testing 123', () => {
+	cy.visit('example.cypress.io/todo#/')
+	cy.get('input[class="new-todo"]').then(($element) => {
+		const elementText = $element.text
+		cy.log(elementText)
+		//elementText.should('not.equal', 'fgdfgdfg')
+		expect(elementText).to.not.equal('fdfgdfg')
+	})
+	})
+	
+	it ('another test', () => {
+		
+		const array1 = [1,2,3]
+		const array2 = [4,2,1]
+		cy.arrayReverseCompare(array1,array2).then( (value) => {
+			expect(value).to.be.true
+		})
+})
+
+})
